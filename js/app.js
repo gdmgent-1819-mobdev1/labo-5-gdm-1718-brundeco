e78ca8cf13c8e8c511e33b43246028744d51e7a0
Bruno De Coene <brunodecoene@Brunos-MacBook-Pro-2.local>
2018-11-03 23:23:36 +0100

    Beter

--- js/app.js
+++ js/app.js
@@ -1,9 +1,4 @@
-let message = document.getElementById('message');
-let message2 = document.getElementById('message2');
-message.setAttribute('class', 'container margin-top');
-
 (function() {
-
   const config = {
     apiKey: "AIzaSyBWMEIJDFgh1SA5TFEZ3ntO7HX-L49Ptpc",
     authDomain: "labo-5-bd772.firebaseapp.com",
@@ -14,23 +9,27 @@ message.setAttribute('class', 'container margin-top');
   };
   firebase.initializeApp(config);
 
-
+  // creating our variables
   const txtEmail = document.getElementById('txtEmail');
   const txtPassword = document.getElementById('txtPassword');
   const btnLogin = document.getElementById('btnLogin');
   const btnSignup = document.getElementById('btnSignup');
   const btnLogout = document.getElementById('btnLogout');
-  const btnLogout2 = document.getElementById('btnLogout2');
+
+  const loginForm = document.getElementById('loginForm');
+  const succesForm = document.getElementById('succesForm');
+  const blogForm = document.getElementById('blogForm');
+
+  const message = document.getElementById('message');
+  const message2 = document.getElementById('message2');
 
   btnLogin.addEventListener('click', e => {
     const email = txtEmail.value;
     const pass = txtPassword.value;
     const auth = firebase.auth();
-  
     const promise = auth.signInWithEmailAndPassword(email, pass);
-    promise.then();
-    promise.catch(e => message.innerHTML = 'Fail');
-
+    promise.then(loginSuccessful);
+    promise.catch(e => message.innerHTML = e.message);
   });
 
   btnSignup.addEventListener('click', e => {
@@ -39,27 +38,29 @@ message.setAttribute('class', 'container margin-top');
     const auth = firebase.auth();
 
     const promise = auth.createUserWithEmailAndPassword(email, pass);
-    promise.then(registerSuccesfull, message.innerHTML = 'Welcome, you registered succesfully with ' + email + '!');
-    promise.catch(e => message.innerHTML = 'Fail');
+    promise.then(registerSuccessful);
+    promise.catch(e => message.innerHTML = e.message);
   });
 
   btnLogout.addEventListener('click', e => {
-    firebase.auth().signOut();
-    message.innerHTML = 'Logout succesful';
+    firebase.auth().signOut().then(function() {
+      location.reload();
+      console.log(firebaseUser);
+    });
   });
-
-
-
 
   firebase.auth().onAuthStateChanged(firebaseUser => {
     if(firebaseUser) {
+      let email = txtEmail.value;
       // console.log(firebaseUser);
-      btnLogout.classList.remove('hide');
-
+      console.log(email);
+      loginForm.style.display = 'none';
+      btnLogout.style.visibility = 'visible';
+      succesForm.style.display = 'block';
+      blogForm.style.display = 'block';
+      message2.innerHTML += 'Welcome ' + email;
     } else {
-
       console.log('Not logged in');
-      btnLogout.classList.add('hide');
     }
   })
 
--- js/notification.js
+++ js/notification.js
@@ -1,40 +1,12 @@
-Notification.requestPermission().then(function(result) {
-  console.log(result);
-});
-
-function registerSuccesfull() {
-  const email = document.getElementById('txtEmail').value;
-  const text = 'You are now registered with ' + email;
-  if (!("Notification" in window)) {
-    alert("This browser does not support system notifications");
-  }
-
-  else if (Notification.permission === "granted") {
-    let notification = new Notification("Succes", {body: text});
-  }
-
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
-      if (permission === "granted") {
-        let notification = new Notification("Succes", {body: text});
-      }
-    });
-  }
-}
-
-function loginSuccesfull() {
+function loginSuccessful() {
   const email = document.getElementById('txtEmail').value;
   const text = 'You are now logged in with ' + email;
-  // const currentUser = document.getElementById('currentUser');
-  // currentUser.innerHTML = 'LOGGED IN AS ' + '<br>' + email;
   if (!("Notification" in window)) {
     alert("This browser does not support system notifications");
   }
-
   else if (Notification.permission === "granted") {
     let notification = new Notification("Welcome", {body: text});
   }
-
   else if (Notification.permission !== 'denied') {
     Notification.requestPermission(function (permission) {
       if (permission === "granted") {
@@ -42,7 +14,22 @@ function loginSuccesfull() {
       }
     });
   }
+}
 
-
-
+function registerSuccessful() {
+  const email = document.getElementById('txtEmail').value;
+  const text = 'You are now registered with ' + email;
+  if (!("Notification" in window)) {
+    alert("This browser does not support system notifications");
+  }
+  else if (Notification.permission === "granted") {
+    let notification = new Notification("Welcome!", {body: text});
+  }
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      if (permission === "granted") {
+        let notification = new Notification("Welcome!", {body: text});
+      }
+    });
+  }
 }
