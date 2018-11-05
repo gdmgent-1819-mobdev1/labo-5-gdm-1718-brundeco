75e4da47d3b108d75029a4267331ec775760ba53
Bruno De Coene <brunodecoene@Brunos-MacBook-Pro-2.local>
2018-11-05 03:08:54 +0100

    CKE editor

--- js/blog.js
+++ js/blog.js
@@ -20,10 +20,9 @@ function writeBlogPost() {
     
     // Collect the values from the form inputfields
     const blogTitle = document.getElementById('blogTitle').value;
-    const blogContent = document.getElementById('blogBody').value;
+    const blogContent = CKEDITOR.instances.blogBody.getData();
     const blogAuthor = localStorage.getItem('currentUser');
     const blogDate = today;
-    console.log(blogBody);
 
      // Put form data in a blogdata oject
     let blogData = {
@@ -36,23 +35,18 @@ function writeBlogPost() {
     ref.push(blogData);
     console.log(ref);
 }
-btnPost.addEventListener('click', writeBlogPost);
+    // add event listener to post button
+    btnPost.addEventListener('click', writeBlogPost);
 
-// get the elements to append blogdata to
-const displayBlogTitle = document.getElementById('displayBlogTitle');
-const displayBlogContent = document.getElementById('displayBlogContent');
-const displayBlogDate = document.getElementById('displayBlogDate');
-const displayBlogAuthor = document.getElementById('displayBlogAuthor');
-const blogResults = document.getElementById('blogResults');
 
 // listen to the value event of ref element
 ref.on('value', gotData, errData);
 
 function gotData(data) {
-    // console.log(data.val());
     let posts = data.val();
     let keys = Object.keys(posts);
 
+    // loop through contents of each post
     for(let i = 0; i < keys.length; i++) {
         let k = keys[i];
         let titles = posts[k].title;
@@ -60,16 +54,13 @@ function gotData(data) {
         let dates = posts[k].date;
         let authors = posts[k].author;
 
-        console.log(titles);
-        console.log(authors);
-        console.log(dates);
-        console.log(contents);
-
+        // create a main container
         let mainDiv = document.createElement('div');
         blogResults.appendChild(mainDiv);
 
+        // create variables that hold the blogpost object data
         let displayBlogTitle = document.createElement('h5').innerHTML = titles;
-        let displayBlogContent = document.createElement('h5').innerHTML = contents[0];
+        let displayBlogContent = document.createElement('h5').innerHTML = contents;
         let displayBlogDate = document.createElement('h5').innerHTML = dates;
         let displayBlogAuthor = document.createElement('h5').innerHTML = authors;
         
@@ -78,6 +69,7 @@ function gotData(data) {
         console.log(displayBlogDate);
         console.log(displayBlogAuthor);
 
+        // try to append childnodes to parentnodes --- > ERROR
         mainDiv.appendChild(displayBlogTitle);
         mainDiv.appendChild(displayBlogContent);
         mainDiv.appendChild(displayBlogDate);
